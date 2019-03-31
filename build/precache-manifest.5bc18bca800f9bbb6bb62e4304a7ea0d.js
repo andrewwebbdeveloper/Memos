02efa8b724746c445ebbb5dbffedacde26ccfebd
Andrew Webb <andrew@Andrews-MacBook-Pro.local>
2019-03-31 12:13:17 -0400

    adding build branch

---
+++ build/precache-manifest.5bc18bca800f9bbb6bb62e4304a7ea0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7eea58ab40aa982016d24b78dd5a7ce",
+    "url": "/static/media/leaves.b7eea58a.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df6e0ba557801e8fda2f",
+    "url": "/static/js/main.24e500db.chunk.js"
+  },
+  {
+    "revision": "d8598bd037eed5ef5ee3",
+    "url": "/static/js/2.d7075f06.chunk.js"
+  },
+  {
+    "revision": "df6e0ba557801e8fda2f",
+    "url": "/static/css/main.010a5142.chunk.css"
+  },
+  {
+    "revision": "d8598bd037eed5ef5ee3",
+    "url": "/static/css/2.3a1aad94.chunk.css"
+  },
+  {
+    "revision": "45c81f3445feac428ac939cc521acde6",
+    "url": "/index.html"
+  }
+];
